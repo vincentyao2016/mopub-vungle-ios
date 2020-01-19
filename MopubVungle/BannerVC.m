1c64f3b4463151e293f0127ff2810516fe22be95
VincentYao <512135129@qq.com>
2020-01-19 16:28:54 +0800

    add close function to banner

--- MopubVungle/BannerVC.m
+++ MopubVungle/BannerVC.m
@@ -12,6 +12,8 @@
 
 @interface BannerVC () <MPAdViewDelegate>
 @property (nonatomic) MPAdView *bannerView, *mrecView;
+@property (nonatomic) UIButton *loadBannerBtn, *loadMrecBtn, *closeBannerBtn, *closeMrecBtn;
+@property (nonatomic) float btnPostionX;
 @end
 
 @implementation BannerVC
@@ -23,44 +25,86 @@
 
 - (void)createView {
     self.view.backgroundColor = UIColor.whiteColor;
+    self.btnPostionX = (self.view.bounds.size.width - 320) / 2;
     
-    float x = (self.view.bounds.size.width - 320) / 2;
+    self.loadBannerBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    self.loadBannerBtn.frame = CGRectMake(self.btnPostionX, 80, 320, 60);
+    [self.loadBannerBtn setTitle:@"Load & Play Banner" forState:UIControlStateNormal];
+    [self.loadBannerBtn addTarget:self action:@selector(loadBanner) forControlEvents:UIControlEventTouchUpInside];
     
-    // Create MPAdView instance
-    self.bannerView = [[MPAdView alloc] initWithAdUnitId:bannerPlacement];
-    self.bannerView.delegate = self;
-    self.bannerView.frame = CGRectMake(x, 200, 320, kMPPresetMaxAdSize50Height.height);
-    self.bannerView.backgroundColor = UIColor.orangeColor;
+    self.closeBannerBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    self.closeBannerBtn.frame = CGRectMake(self.btnPostionX, 120, 320, 60);
+    [self.closeBannerBtn setTitle:@"Close Banner" forState:UIControlStateNormal];
+    [self.closeBannerBtn addTarget:self action:@selector(closeBanner) forControlEvents:UIControlEventTouchUpInside];
     
-    self.mrecView = [[MPAdView alloc] initWithAdUnitId:mrecPlacement];
-    self.mrecView.delegate = self;
-    self.mrecView.frame = CGRectMake(x, 400, 300, 250);
-    self.mrecView.backgroundColor = UIColor.orangeColor;
+    self.loadMrecBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    self.loadMrecBtn.frame = CGRectMake(self.btnPostionX, 280, 320, 60);
+    [self.loadMrecBtn setTitle:@"Load & Play MREC" forState:UIControlStateNormal];
+    [self.loadMrecBtn addTarget:self action:@selector(loadMrec) forControlEvents:UIControlEventTouchUpInside];
     
-    UIButton *loadBanner = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    loadBanner.frame = CGRectMake(x, 120, 320, 80);
-    [loadBanner setTitle:@"Load & Play Banner" forState:UIControlStateNormal];
-    [loadBanner addTarget:self action:@selector(loadBanner) forControlEvents:UIControlEventTouchUpInside];
+    self.closeMrecBtn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    self.closeMrecBtn.frame = CGRectMake(self.btnPostionX, 320, 320, 60);
+    [self.closeMrecBtn setTitle:@"Close MREC" forState:UIControlStateNormal];
+    [self.closeMrecBtn addTarget:self action:@selector(closeMrec) forControlEvents:UIControlEventTouchUpInside];
     
-    UIButton *loadMrec = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-    loadMrec.frame = CGRectMake(x, 320, 320, 80);
-    [loadMrec setTitle:@"Load & Play MREC" forState:UIControlStateNormal];
-    [loadMrec addTarget:self action:@selector(loadMrec) forControlEvents:UIControlEventTouchUpInside];
-    
-    [self.view addSubview:loadBanner];
-    [self.view addSubview:self.bannerView];
-    [self.view addSubview:loadMrec];
-    [self.view addSubview:self.mrecView];
+    [self.view addSubview:self.loadBannerBtn];
+    [self.view addSubview:self.closeBannerBtn];
+    [self.view addSubview:self.loadMrecBtn];
+    [self.view addSubview:self.closeMrecBtn];
 }
 
 #pragma mark - load & play
+- (void)createBanner {
+    // Create MPAdView instance
+    self.bannerView = [[MPAdView alloc] initWithAdUnitId:bannerPlacement];
+    self.bannerView.delegate = self;
+    self.bannerView.frame = CGRectMake(self.btnPostionX, 200, 320, kMPPresetMaxAdSize50Height.height);
+    self.bannerView.backgroundColor = UIColor.orangeColor;
+    [self.view addSubview:self.bannerView];
+}
+
 - (void)loadBanner {
     // automatically refreshes an ad unit at a time interval specified via the MoPub web interface
+    [self createBanner];
     [self.bannerView loadAdWithMaxAdSize:kMPPresetMaxAdSize50Height];
+    self.loadBannerBtn.enabled = NO;
+    self.loadMrecBtn.enabled = NO;
+}
+
+- (void)closeBanner {
+   if (self.bannerView) {
+       [self.bannerView removeFromSuperview];
+       self.bannerView.delegate = nil;
+       self.bannerView = nil;
+       self.loadBannerBtn.enabled = YES;
+       self.loadMrecBtn.enabled = YES;
+   }
+}
+
+- (void)createMrec {
+    self.mrecView = [[MPAdView alloc] initWithAdUnitId:mrecPlacement];
+    self.mrecView.delegate = self;
+    self.mrecView.frame = CGRectMake(self.btnPostionX, 400, 300, 250);
+    self.mrecView.backgroundColor = UIColor.orangeColor;
+    [self.view addSubview:self.mrecView];
 }
 
 - (void)loadMrec {
+    // automatically refreshes an ad unit at a time interval specified via the MoPub web interface
+    [self createMrec];
     [self.mrecView loadAdWithMaxAdSize:kMPPresetMaxAdSize250Height];
+    self.loadBannerBtn.enabled = NO;
+    self.loadMrecBtn.enabled = NO;
+}
+
+- (void)closeMrec {
+    if (self.mrecView) {
+        [self.mrecView removeFromSuperview];
+        self.mrecView.delegate = nil;
+        self.mrecView = nil;
+        self.loadBannerBtn.enabled = YES;
+        self.loadMrecBtn.enabled = YES;
+    }
 }
 
 #pragma mark - MPAdView Delegate
